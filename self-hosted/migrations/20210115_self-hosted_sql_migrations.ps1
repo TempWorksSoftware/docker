5f3d3a85f94995582ef72919a1f9b1a97bdddc2c
Erik Anderson <erikbpanderson@gmail.com>
2021-01-15 00:07:39 -0600

    Revised 1/15 ApiScopes

---
+++ self-hosted/migrations/20210115_self-hosted_sql_migrations.ps1
@@ -0,0 +1,81 @@
+param (
+    # Path to the login-server configuration directory
+    [ValidateScript(
+         {
+             if (-Not (Test-Path -Path $_)) {
+                 throw "Login Server service config directory ($_) not found!"
+             }
+             return $true
+         })]
+    [string]
+    $loginServer = $(
+        if (-Not (Test-Path -Path "C:\ProgramData\TempWorks\config\login-server")) {
+            throw "Login Server service config directory (C:\ProgramData\TempWorks\config\login-server) not found!"
+        }
+        return "C:\ProgramData\TempWorks\config\login-server"
+    )
+)
+
+$appsettingsPath = $loginServer+'\appsettings.production.json'
+
+$loginServerConnectionString = (Get-Content ($appsettingsPath) -ErrorAction stop | Out-String |ConvertFrom-Json).ConnectionStrings.TwLoginServerDatabase
+
+function Execute-SqlQuery ($connectionString, $query) {
+    $connection = New-Object System.Data.SqlClient.SQLConnection "$connectionString"
+    $connection.ConnectionString=$connectionString
+    $connection.Open()
+    $cmd = New-Object system.Data.SqlClient.SqlCommand($query, $connection)
+    $ds = New-Object system.Data.DataSet
+    $da = New-Object system.Data.SqlClient.SqlDataAdapter $cmd
+    [void]$da.fill($ds)
+    $connection.Close()
+    return $ds.Tables
+}
+
+function Apply-SqlMigration ($connectionString, $description, $testQuery, $updateQuery) {
+    $testResult = Execute-SqlQuery -ConnectionString $connectionString -Query $testQuery
+    
+    if ($testResult.Table) {
+        Write-Host "'$description' already applied." -foregroundcolor green
+    } else {
+        Write-Host "Applying '$description'..." -NoNewline
+
+        try {                        
+            Execute-SqlQuery $connectionString $updateQuery
+            Write-Host " SUCCEEDED." -foregroundcolor green
+        } catch [Exception] {
+            Write-Host " FAILED." -foregroundcolor red
+            Write-Error -Message "Could not apply SQL migration"
+            Write-Error -Exception $_.Exception
+            Write-Host "Consider applying this SQL migration manually:"
+            Write-Host "----------------------------------------"
+            write-host $updateQuery
+            Write-Host "----------------------------------------"
+            Write-Error -Message "Could not apply SQL migration"
+            Write-Error -Exception $_.Exception
+        }
+    }
+}
+
+$additionalScopes = @(@{ApiResourceId = 1; Description = "Allow read access to Sovren data"; DisplayName = "Sovren SaaS Read"; Emphasize = 0; Name = "sovrensaas-read"; Required = 0; ShowInDiscoveryDocument = 0;},
+                      @{ApiResourceId = 1; Description = "Allow webhook access for Sovren"; DisplayName = "Sovren SaaS Webhook"; Emphasize = 0; Name = "sovrensaas-webhook"; Required = 0; ShowInDiscoveryDocument = 0;},
+                      @{ApiResourceId = 1; Description = "Allow write access to Sovren data"; DisplayName = "Sovren SaaS Write"; Emphasize = 0; Name = "sovrensaas-write"; Required = 0; ShowInDiscoveryDocument = 0;},
+                      @{ApiResourceId = 1; Description = "Allow read access to Email Accounts and Email Search"; DisplayName = "Email Read"; Emphasize = 0; Name = "email-read"; Required = 0; ShowInDiscoveryDocument = 1;},
+                      @{ApiResourceId = 1; Description = "Allow write access to Email Accounts and Email Sending"; DisplayName = "Email Write"; Emphasize = 0; Name = "email-write"; Required = 0; ShowInDiscoveryDocument = 1;})
+
+$additionalScopes | % {
+    $sqlMigrationTestQuery = @"
+SELECT 1 from dbo.ApiScopes WHERE name LIKE '$($_.Name)'
+"@
+    $sqlMigrationQuery = @"
+IF NOT EXISTS (SELECT 1 from dbo.ApiScopes WHERE name LIKE '$($_.Name)')
+BEGIN
+PRINT N'Inserting ApiScope "$($_.Name)"...';
+
+INSERT INTO ApiScopes (ApiResourceId, Description, DisplayName, Emphasize, Name, Required, ShowInDiscoveryDocument)
+VALUES ($($_.ApiResourceId), '$($_.Description)', '$($_.DisplayName)', $($_.Emphasize), '$($_.Name)', $($_.Required), $($_.ShowInDiscoveryDocument))
+END
+
+"@
+    Apply-SqlMigration $loginServerConnectionString "Add $($_.Name) ApiScope to login database" $sqlMigrationTestQuery $sqlMigrationQuery
+}
--- self-hosted/migrations/readme.md
+++ self-hosted/migrations/readme.md
@@ -1,14 +1,15 @@
 # Migration scripts
 
-| Script                                    |    Release | Description                                   |
-| ----------------------------------------- | ----------- | ----------------------------------------------|
-| [20191101_self-hosted_sql_migration.ps1](./20191101_self-hosted_sql_migration.ps1)  | 2019-11-01 | Subtenant functionality added to login server |
-| [20191220_self-hosted_sql_migrations.ps1](./20191220_self-hosted_sql_migrations.ps1) | 2019-12-20 | Additional email schema initialization |
-| [20200306_self-hosted_sql_migrations.ps1](./20200306_self-hosted_sql_migrations.ps1) | 2020-03-06 | Add background checks ApiScope to login database |
-| [20200417_self-hosted_sql_migrations.ps1](./20200417_self-hosted_sql_migrations.ps1) | 2020-04-17 | Subtenant schema changes |
-| [20200807_self-hosted_sql_migrations.ps1](./20200807_self-hosted_sql_migrations.ps1) | 2020-08-07 | Add assessment webhook ApiScope to login database |
-| [20201016_self-hosted_sql_migrations.ps1](./20201016_self-hosted_sql_migrations.ps1) | 2020-10-16 | Add Indeed webhook ApiScope to login database |
-| [20201120_self-hosted_sql_migrations.ps1](./20201120_self-hosted_sql_migrations.ps1) | 2020-11-20 | Add background checks ApiScopes to login database |
+| Script                                                                               | Release     | Description                                       |
+| -----------------------------------------                                            | ----------- | ----------------------------------------------    |
+| [20191101_self-hosted_sql_migration.ps1](./20191101_self-hosted_sql_migration.ps1)   | 2019-11-01  | Subtenant functionality added to login server     |
+| [20191220_self-hosted_sql_migrations.ps1](./20191220_self-hosted_sql_migrations.ps1) | 2019-12-20  | Additional email schema initialization            |
+| [20200306_self-hosted_sql_migrations.ps1](./20200306_self-hosted_sql_migrations.ps1) | 2020-03-06  | Add background checks ApiScope to login database  |
+| [20200417_self-hosted_sql_migrations.ps1](./20200417_self-hosted_sql_migrations.ps1) | 2020-04-17  | Subtenant schema changes                          |
+| [20200807_self-hosted_sql_migrations.ps1](./20200807_self-hosted_sql_migrations.ps1) | 2020-08-07  | Add assessment webhook ApiScope to login database |
+| [20201016_self-hosted_sql_migrations.ps1](./20201016_self-hosted_sql_migrations.ps1) | 2020-10-16  | Add Indeed webhook ApiScope to login database     |
+| [20201120_self-hosted_sql_migrations.ps1](./20201120_self-hosted_sql_migrations.ps1) | 2020-11-20  | Add Background checks ApiScopes to login database |
+| [20210115_self-hosted_sql_migrations.ps1](./20210115_self-hosted_sql_migrations.ps1) | 2021-01-15  | Add Email and Sovren ApiScopes to login database |
 
 To avoid settings and resetting execution policy, these scripts can be invoked in the following manner.  Note that if you have placed your service configuration paths follow a convention other than `C:\ProgramData\TempWorks\config\{service-name}`, you will need to manually supply a path to the service configuration folder.
 
